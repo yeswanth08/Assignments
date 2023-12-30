5c157265de039ed9f0d7de5b90414c06c3be1f42
yeswanth08 <curiousyeswanth003@gmail.com>
2023-12-30 15:24:12 +0530

    DEC-->30

--- week-2/01-async-js/medium/filecleaner.js
+++ week-2/01-async-js/medium/filecleaner.js
@@ -4,7 +4,9 @@ file.readFile('file.txt','utf-8',(err,data)=>{
     if (err){
         console.log("Error had occured\n");
     }else{
-        data.replace(/\s+/g     ,'');
-        console.log(data);
+        const value=data.replace(/\s+/g,' ');
+        file.writeFile('file.txt',value,(err)=>{
+            (err) ? console.log('Error has occured') :  console.log(value);
+        })
     }
 })
